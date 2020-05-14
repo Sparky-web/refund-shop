f540bcc5b7d020a21c89f1ce4dda23730e5ca631
VLadYxa343 <50592770+VLadYxa343@users.noreply.github.com>
2020-05-14 17:22:11 +0500

    First commit

---
+++ precache-manifest.47071b0b272cb2f6f93b22ea7dbbccfd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809c5979b3764e26f269960ca634fe5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb35cccae6fff6bb2e1e",
+    "url": "/static/css/main.271cdcab.chunk.css"
+  },
+  {
+    "revision": "af49062da8eac2f05b2b",
+    "url": "/static/js/2.16ccddd8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.16ccddd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb35cccae6fff6bb2e1e",
+    "url": "/static/js/main.58d26adf.chunk.js"
+  },
+  {
+    "revision": "6191260f7b636a081a73",
+    "url": "/static/js/runtime-main.df2a3e2d.js"
+  },
+  {
+    "revision": "4ad50f58da1fbdd727fb62f31c51e298",
+    "url": "/static/media/aliexpress.4ad50f58.jpg"
+  },
+  {
+    "revision": "e7952e3170ddac9780180a5e9884262d",
+    "url": "/static/media/bg1.e7952e31.jpg"
+  },
+  {
+    "revision": "27ec41e2cd13117e2011679a5b296cc4",
+    "url": "/static/media/bg2.27ec41e2.jpg"
+  },
+  {
+    "revision": "a7f8fe51a519549de58690f880692427",
+    "url": "/static/media/bg3.a7f8fe51.jpg"
+  },
+  {
+    "revision": "b9c7a55559c4aa211a1226130a5608fc",
+    "url": "/static/media/bg4.b9c7a555.jpg"
+  },
+  {
+    "revision": "fb0b287fd7945e9feecc44c399c78cea",
+    "url": "/static/media/bg5.fb0b287f.jpg"
+  },
+  {
+    "revision": "ecb675d8b25d24f10bc71987efa4c9de",
+    "url": "/static/media/headline.ecb675d8.svg"
+  }
+]);
